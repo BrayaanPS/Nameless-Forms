121a9c212161c445b7591bb443fc2aba64c71e7e
BrayaanPS <30274810+BrayaanPS@users.noreply.github.com>
2020-11-19 14:32:24 -0300

    Create Spanish.php

---
+++ upload/modules/Forms/language/Spanish.php
@@ -0,0 +1,102 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  https://github.com/partydragen/Nameless-Forms
+ *  https://partydragen.com/
+ *  NamelessMC version 2.0.0-pr6
+ *
+ *  License: MIT
+ *
+ *  Spanish Language for Forms module
+ */
+
+$language = array(
+	// Forms
+	'forms' => 'Formularios',
+	'form' => 'Formulario',
+	'new_form' => 'Nuevo formulario',
+	'form_name' => 'Nombre del formulario',
+	'form_url' => 'URL del Formulario (con el precedente /, ejemplo: /ejemplo)',
+	'form_icon' => 'Icono del formulario',
+	'link_location' => 'Ubicación del enlace',
+	'creating_new_form' => 'Crear un nuevo formulario',
+	'form_created_successfully' => 'Formulario creado con éxito',
+	'none_forms_defined' => 'Todavía no hay formularios.',
+	'delete_form' => '¿Estás seguro de que quieres borrar este formulario?</br>Advertencia: Todos los datos que pertenezcan a este formulario serán eliminados como preguntas y presentaciones',
+	'form_submitted' => 'Formulario presentado con éxito',
+	'action' => 'Acción',
+	'actions' => 'Acciones',
+	'guest' => 'Visita',
+	
+	// Permissions
+	'forms_view_submissions' => 'StaffCP &raquo; Formularios &raquo; Presentaciones',
+	'forms_manage' => 'StaffCP &raquo; Formularios &raquo; Formularios',
+	
+	// Form
+	'editing_x' => 'Editing {x}', // Don't replace {x}
+	'form_created_successfully' => 'Formulario creado con éxito.',
+	'form_updated_successfully' => 'Formulario actualizado con éxito.',
+	'form_deleted_successfully' => 'Formulario eliminado con éxito.',
+	'allow_guests' => '¿Pueden las visitas ver este formulario?',
+	'allow_guests_help' => 'Los invitados podrán enviar sus propuestas sin necesidad de registrarse. Por favor, tenga en cuenta que no podrán ver la propuesta después.',
+	'can_user_view' => '¿El usuario puede ver su propia presentación?',
+	'can_user_view_help' => 'El usuario podrá ver su propio envío y usar la sección de comentarios. El usuario también recibirá alertas de actualización cuando el estado cambie o cuando alguien haga un comentario. Por favor, ten en cuenta que esto no funcionará para los invitados.',
+	
+	// Fields
+	'field' => 'Campo',
+	'fields' => 'Campos',
+	'new_field' => 'Nuevo campo',
+	'field_name' => 'Nombre del campo',
+	'field_created_successfully' => 'El campo creado con éxito',
+	'field_updated_successfully' => 'El campo se actualizó con éxito',
+	'field_deleted_successfully' => 'El campo fue eliminado con éxito',
+	'new_field_for_x' => 'Creando un nuevo campo para {x}',
+	'editing_field_for_x' => 'Campo de edición para {x}',
+	'none_fields_defined' => 'No hay campos todavía.',
+	'confirm_delete_field' => '¿Estás seguro de que quieres borrar este campo?',
+	'options' => 'Options',
+	'options_help' => 'Cada opción en una nueva línea; puede dejarse vacía (sólo opciones)',
+	'field_order' => 'Orden de campo',
+	'delete_field' => '¿Estás seguro de que quieres borrar este campo?',
+	
+	// Estatus
+	'statuses' => 'Estatus',
+	'status' => 'Estado',
+	'new_status' => 'Nuevo estado',
+	'creating_status' => 'Creando un nuevo estatus',
+	'editing_status' => 'Editando estado',
+	'marked_as_open' => 'Marcado como abierto',
+	'status_name' => 'Nombre del estado',
+	'status_html' => 'Estado HTML',
+	'status_forms' => 'Formularios de estado',
+	'status_groups' => 'Grupos de estatus',
+	'status_creation_success' => 'Estado creado con éxito.',
+	'status_creation_error' => 'Error al crear un estado. Por favor, asegúrese de que el html de estado no tenga más de 1024 caracteres.',
+	'status_edit_success' => 'Estado editado con éxito.',
+	'status_deleted_successfully' => 'Estado eliminado con éxito.',
+	'delete_status' => '¿Estás seguro de que quieres borrar este estado?',
+
+	// Errores
+	'input_form_name' => 'Por favor, introduzca un nombre de formulario.',
+	'input_form_url' => 'Por favor, introduzca un formulario url.',
+	'form_name_minimum' => 'El nombre del formulario debe tener un mínimo de 2 caracteres.',
+	'form_url_minimum' => 'El url debe ser de un mínimo de 2 caracteres.',
+	'form_name_maximum' => 'El nombre del formulario debe tener un máximo de 32 caracteres.',
+	'form_url_maximum' => 'El url debe ser de un máximo de 32 caracteres.',
+	'form_icon_maximum' => 'El icono del formulario debe tener un máximo de 64 caracteres.',
+	'input_field_name' => 'Por favor, introduzca un nombre en el campo.',
+	'field_name_minimum' => 'El nombre del campo debe tener un mínimo de 2 caracteres.',
+	'field_name_maximum' => 'El nombre del campo debe tener un máximo de 255 caracteres.',
+	
+	// Submissions
+	'submissions' => 'Presentaciones',
+	'submission_updated' => 'Presentación actualizada con éxito',
+	'no_open_submissions' => 'Actualmente no hay presentaciones abiertas.',
+	'no_closed_submissions' => 'Actualmente no hay presentaciones cerradas.',
+	'form_x' => 'Formulario: {x}',
+	'current_status_x' => 'Estado actual: {x}',
+	'last_updated' => 'Última actualización:',
+	'your_submission_updated' => 'Su presentación ha sido actualizada',
+	'user' => 'Usuario',
+	'updated_by' => 'Actualizado por',
+);
